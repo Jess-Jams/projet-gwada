1ca0dd11dedddf61d6d9ee5c92b48f0dd80c2e2d
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2022-09-20 22:27:37 +0200

    Bonjour la Guadeloupe

--- pusher-slyder.sh
+++ pusher-slyder.sh
@@ -4,7 +4,7 @@ echo "Starting to save your code"
 git add .
 echo "Your code is cached ( il est en mémoire volatile )"
 echo "Waiting for commit ( mais on a besoin d'un message )"
-read -p "What is the message ?" msg
+read -p "What is the message ? " msg
 git commit -m "$msg"
 echo "Your code has been comited with the message : $msg"
 git push
